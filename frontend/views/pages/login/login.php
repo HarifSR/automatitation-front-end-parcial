6d79b79efed307bbb1e14fe21f771cfdbe7dff02
Harif3562 <148810485+HarifSR@users.noreply.github.com>
2025-03-08 15:19:17 -0600

    Login form

--- frontend/views/pages/login/login.php
+++ frontend/views/pages/login/login.php
@@ -1,2 +1,15 @@
 <div class="dark-background" id="dark-background">
     <div class="form-container">
+
+    <section class="section section__form--log-in visible">
+            <form action="" class="log-in">
+                <h1 class="title__h1">Log in</h1>
+                <input placeholder="username" type="text" class="username" id="username-log-in">
+                <input placeholder="password" type="password" class="password" id="password-log-in">
+                <a href="#" class="link">Forgot your password?</a>
+                <button class="button button__form" id="button-log-in">Log in</button>
+                <button class="button button__form--switch" type="button" onclick="switchForm('signup')">
+                    Sign up
+                </button>
+            </form>
+        </section>
